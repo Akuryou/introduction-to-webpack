b7cd679a63cc0ee174e034b07f92ce0ec0702c6a
akuryou <akuryou@byteweaver.org>
2017-05-15 20:07:22 +0200

    rework prod config using webpack-merge

--- webpack.production.js
+++ webpack.production.js
@@ -1,11 +1,30 @@
 const webpack = require('webpack');
+const merge = require('webpack-merge');
 const base = require('./webpack.config');
 
-base.plugins.push(new webpack.optimize.UglifyJsPlugin({
-  beautify: false,
-  mangle: false,
-  compress : { screw_ie8 : true },
-  comments: false,
-}));
-
-module.exports = base;
+module.exports = function(env) {
+  return merge(base, {
+    plugins: [
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false
+      }),
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        beautify: false,
+        mangle: {
+          screw_ie8: true,
+          keep_fnames: true
+        },
+        compress: {
+          screw_ie8: true
+        },
+        comments: false
+      })
+    ]
+  })
+}
